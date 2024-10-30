7557f98cdae5ff61bb1d60422560d0da262db139
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-10-30 15:07:16 +0100

    Création du controller pour les inscription & connexion

---
+++ app/src/controllers/Auth.controller.js
@@ -0,0 +1,31 @@
+const User = require('../models/User.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
+exports.register = async (req, res) => {
+    try{
+        const { email, username, password} = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({email, username, password:hashedPassword})
+        res.status(201).json({ message: 'Utilisateur créé', user})
+    }
+    catch(err){
+        res.status(500).json({ error: 'Erreur lors de l’inscription' });
+    }
+}
+
+exports.login = async (req, res) => {
+    try{
+        const {email, password} = req.body
+        const user = await User.findOne({ where: {email} })
+        if(!user|| !(user.bcrypt.compare(password, user.password))){
+            return res.status(401).json({ error: 'Identifiants incorrects' });
+        }
+        const token = jwt.sign({id: user.id}, config.secret, {expiresIn:'1h'})
+        res.json({ message: 'Connexion réussie', token });
+    }
+    catch(err){
+        res.status(500).json({ error: 'Erreur lors de la connexion' });
+    }
+}
