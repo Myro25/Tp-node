c7b1a79f5b9b1a5be683b1788545013472890816
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-10-30 12:07:56 +0100

    Mise en place de la connexion à la bdd

---
+++ app/src/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+module.exports = {
+    db:{
+        name: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST
+    },
+    secret: process.env.SECRET,
+}
